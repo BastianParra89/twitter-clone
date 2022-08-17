5de6452136e44154c85fd27f3d1155d87b97fc68
bastianparra89 <bastianpb@gmail.com>
2022-08-16 20:19:00 -0400

    first commit

---
+++ twitterclone/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import Sidebar from './Sidebar';
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>BARRA LATERAL TWITTER</h1>
+
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
